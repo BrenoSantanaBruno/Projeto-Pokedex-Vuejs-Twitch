2123a7ec2d4865fc7edab634dd3986d53dbfdd6b
Breno Santana <44840815+LinuxLoverCoder@users.noreply.github.com>
2022-08-14 16:40:35 -0300

    Second Commit
    Starting Project

---
+++ pokedex/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="row mt-2">
+      <div class="col mb-2">
+        Palco
+      </div>
+      <div class="col mb-2">
+        Pokédex
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+
+  }
+}
+</script>
